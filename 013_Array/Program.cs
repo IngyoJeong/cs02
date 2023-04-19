f2a1df0d71cf7269c1f6750e47b68386439506b3
IngyoJeong <127263653+IngyoJeong@users.noreply.github.com>
2023-04-19 09:40:48 +0900

    Add files via upload

---
+++ 013_Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //10개짜리 정수 배열을 만들자
+            int[] a = new int[10];
+
+            //10개의 숫자를 입력 받아서 배열에 저장
+            for (int i = 0; i < 10; i++)
+                a[i] = int.Parse(Console.ReadLine());
+
+            //배열에 저장된 10개의 숫자를 출력하자
+            for (int i = 0; i < 10; i++)
+                Console.Write(a[i] + " ");
+            Console.WriteLine();
+
+            foreach (var x in a)
+                Console.Write(x + " ");
+            Console.WriteLine();
+
+            //문자열을 5개 저장할 수 있는 배열을 만들고 입력받은 후 출력하라
+            string[] b = new string[5];
+            for (int i = 0; i < 5; i++)
+                b[i] = Console.ReadLine();
+            foreach (var y in b)
+                Console.Write(y);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 014_Class/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            //랜덤 숫자 5개를 출력
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(r.Next(100));
+
+            //숫자 20개를 랜덤하게 만들어 배열에 저장한 후 최소, 최대, 평균값 계산
+            int[] a = new int[20];
+            for (int i = 0; i < 20; i++)
+                a[i] = r.Next(100);
+
+            Console.WriteLine("배열의 내용 : ");
+            foreach (var x in a)
+                Console.Write(x + " ");
+            Console.WriteLine();
+
+            int min = a[0];
+            int max = a[0];
+            int sum = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (a[i] < min)
+                    min = a[i];
+                if (a[i] > max)
+                    max = a[i];
+                sum += a[i];
+            }
+            Console.WriteLine("최솟값 = {0}, 최댓값 = {1}, 평균 = {2}", min, max, sum / 20.0);
+        }
+    }
+}
